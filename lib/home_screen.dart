a9bd660c901c99abb21d7dcd679c3759ebf2de83
Tmkantichaaaa03 <momonut6627@gmail.com>
2025-02-14 10:28:19 +0700

    Add files via upload

---
+++ lib/home_screen.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+ 
+class HomeScreen extends StatefulWidget{
+  /* const _HomeScreen({super.key});*/
+  @override
+  State<StatefulWidget> createState() {
+    return _HomeScreen();
+  }
+ 
+}
+class _HomeScreen extends State<HomeScreen>{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('SHOP', style: TextStyle(letterSpacing: 0.5),
+          ),
+          backgroundColor: Colors.white,
+          actions: [
+            Container(
+              margin: EdgeInsets.only(right: 18),
+              child: (Icon.person_2_outlined),
+            ),
+          ] 
+          ),
+          body: Container(),
+      ),
+    );
+  }
+}
